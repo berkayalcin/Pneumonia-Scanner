69948707e0e7c430a510ff8f44c9c20059fbbe05
Berkay YALÇIN <berkay.yalcin@dekopasaj.com>
2020-04-26 19:10:06 +0300

    Initial

---
+++ ChestScan/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChestScan.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ChestScan/wwwroot/js/main.js
@@ -0,0 +1,33 @@
+﻿
+$(document).ready(function () {
+    start();
+});
+async function start() {
+    tf.ENV.set('WEBGL_PACK', false)
+    const model = await tf.loadLayersModel('https://localhost:44329/model/model.json');
+    console.log(model);
+    tf.tidy(() => {
+        const IMAGE_SIZE = 224;
+
+        var image = tf.browser.fromPixels(document.getElementById('image1')).toFloat();
+        const offset = tf.scalar(127.5);
+        const normalized = image.sub(offset).div(offset);
+        const batched = normalized.reshape([1, IMAGE_SIZE, IMAGE_SIZE, 3]);
+        const prediction = model.predict(batched);
+        const tensorData = prediction.dataSync();
+        const normalProb = tensorData[0];
+        const anormalProb = tensorData[1];
+        if (normalProb < 0.5) {
+            $('#resultLabel').addClass("text-danger");
+            $('#resultLabel').html('Zature Belirtisi.');
+        } else {
+            $('#resultLabel').addClass("text-success");
+            $('#resultLabel').html('Zature Belirtisi Bulunmadı.');
+        }
+        $('#resultLabel').removeAttr('hidden');
+        $('#resultLabel').removeProp('hidden');
+        console.log(tensorData);
+    });
+   
+    //console.log(prediction);
+}
